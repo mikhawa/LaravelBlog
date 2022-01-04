933ed0b8af63ad5892bfed31d48641969bd240ad
mikhawa <michaeljpitz@gmail.com>
2022-01-04 15:53:37 +0100

    begin V2

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 Version 8 de Laravel pour suivre le tuto version blog de https://www.udemy.com/course/le-guide-de-laravel-8
 
 Pour revoir le début, cliquez sur ce lien : https://github.com/mikhawa/LaravelBlog/tree/V1
+
+## V2
+
+### Le Contrôleur Article
+
+Dans `routes\web.php` nous n'avons plus que
+
+    use App\Http\Controllers\ArticleController;
+    use Illuminate\Support\Facades\Route;
+
+    // contrôleur ressource
+    Route::resource('articles', ArticleController::class);
+
+Qui appelle `app\Http\Controllers\ArticleController.php`
--- routes/web.php
+++ routes/web.php
@@ -1,96 +1,10 @@
 <?php
-// Appel du contrôleur
 
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\UniqueActionController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
 
-// remplacement par le contrôleur MainController
-Route::get('/', [MainController::class, 'index']);
-Route::get('/testview', [MainController::class, 'test']);
-
-// contrôleur unique
-Route::get('/unique', UniqueActionController::class);
 
 // contrôleur ressource
 Route::resource('articles', ArticleController::class);
-
-
-/*
-// idem que pour le précédent
-Route::get('/testview', function () {
-    return view('testview');
-});
-*/
-
-
-
-Route::get('/testviews/{id}', function ($id) {
-    return view('testviews', [
-        'id' => $id
-    ]);
-});
-
-Route::get('/testviewinfolder', function () {
-    return view('folder.testview2');
-});
-
-Route::get('/env', function () {
-    dd(env('DB_DATABASE'));
-});
-
-Route::get('/hello', function () {
-    return response("Hello World", 202);
-});
-
-Route::redirect('/redirect', '/hello');
-
-Route::any('/tous', function () {
-    return "Est accepté pour toutes les méthodes
-            get, post, put, delete, etc...";
-});
-
-Route::get('/art/{id}', function ($id) {
-    return $id;
-});
-
-Route::get('/art/{id}/comment/{author?}', function ($id, $author = 'Anonyme') {
-    return "$author a écrit un commentaire sur l'article numéro : $id";
-});
-
-Route::prefix('administration')->group(function () {
-    Route::get('user', function () {
-        return "Gestion des utilisateurs";
-    });
-    Route::get('articles', function () {
-        return "Gestion des articles";
-    });
-    Route::get('comment', function () {
-        return "Gestion des commentaires";
-    });
-});
-
-Route::get('/json', function () {
-    return response()->json([
-        'name' => "Michaël",
-        'age' => 44,
-    ]);
-});
