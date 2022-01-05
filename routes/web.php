a629f319d17076ba83e9cd7edf2e0362a47d5071
mikhawa <michaeljpitz@gmail.com>
2022-01-05 12:25:05 +0100

    clean for V3

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,14 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-// contrôleur ressource
-Route::resource('articles', ArticleController::class);
-
-// pour tester un middleware par défaut (guest => non connecté)
-Route::get('/test', function () {
-    return response("Hello World", 200);
-})->middleware('guest');
-
-// pour tester un middleware par défaut (auth.basic => connecté basique)
-Route::get('/test2', function () {
-    return response("Hello World", 200);
-})->middleware('auth.basic');
-
-// pour tester un middleware fait main
-Route::get('/test3', function () {
-    return response("Hello World", 200);
-})->middleware('custom.auth');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
